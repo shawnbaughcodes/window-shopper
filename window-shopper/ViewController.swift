d7756fdc7119ca35efb5c15a8dd37af47d100554
Shawn Baugh II <shawnbaughcodes@gmail.com>
2019-04-19 23:37:11 -0500

    Initial Commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Shawn Baugh on 4/19/19.
+//  Copyright © 2019 Shawn Baugh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
